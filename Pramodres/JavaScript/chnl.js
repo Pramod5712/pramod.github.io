f10cd77f15f891c3253ad574414d80f1b4ce5623
Pramod5712 <joshyt0069@gmail.com>
2022-01-05 22:33:49 +0530

    my first site

---
+++ Pramodres/JavaScript/chnl.js
@@ -0,0 +1,11 @@
+console.log('Hello');
+
+document.getElementById('chmovies')
+    .addEventListener('click', function() {
+        location.href = 'chnlmovies.html'
+    });
+    
+document.getElementById('selfgrowth')
+    .addEventListener('click', function() {
+        location.href = 'chnlselfgrowth.html'
+    });
